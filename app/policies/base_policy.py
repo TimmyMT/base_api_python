28b410fcb2a542cb6cd726066d2f34c373ef06fe
TimmyMT <mathcore_detected@mail.ru>
2026-04-07 12:29:35 +0500

    refactoring

--- app/policies/base_policy.py
+++ app/policies/base_policy.py
@@ -1,6 +1,3 @@
-from fastapi import HTTPException
-from app.models.user import User
-
 class BasePolicy:
     def __init__(self, user: User):
         self.user = user
@@ -15,17 +12,11 @@ class BasePolicy:
         Проверка прав пользователя на категорию по имени.
         action: "read", "create", "update", "delete"
         """
+        field_name = f"can_{action}"  # например "can_read", "can_create"
         for role in self.user.roles:
             for perm in role.permissions:
-                if perm.category.name == category_name:
-                    if action == "read" and perm.can_read:
-                        return True
-                    if action == "create" and perm.can_create:
-                        return True
-                    if action == "update" and perm.can_update:
-                        return True
-                    if action == "delete" and perm.can_delete:
-                        return True
+                if perm.category.name == category_name and getattr(perm, field_name, False):
+                    return True
         return False
 
     # CRUD методы
